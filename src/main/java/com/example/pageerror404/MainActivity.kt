535108792a32ae3fb10971ea3603aa955b63c509
Laila <lailaroberta100@gmail.com>
2021-06-26 15:20:53 -0300

    add function button

--- src/main/java/com/example/pageerror404/MainActivity.kt
+++ src/main/java/com/example/pageerror404/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.pageerror404
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClick = findViewById<Button>(R.id.btn_home)
+
+        btnClick.setOnClickListener {
+            Toast.makeText(applicationContext, "BOTÃO CLICADO", Toast.LENGTH_SHORT).show()
+        }
     }
 }
